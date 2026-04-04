89ca7e10ab3b84f8b753ae942c5e670bea6f2fed
unknown <lyminhhai102@gmail.com>
2026-04-04 22:58:13 +0700

    fix render kh ho tro ipv6

--- charity-donation/backend/src/utils/mailer.js
+++ charity-donation/backend/src/utils/mailer.js
@@ -1,6 +1,11 @@
 const nodemailer = require("nodemailer");
 const env = require("../config/env");
 
+const dns = require("dns");
+
+// Force IPv4 DNS resolution (Render free tier doesn't support IPv6)
+dns.setDefaultResultOrder("ipv4first");
+
 const transporter = nodemailer.createTransport({
   host: env.MAIL_HOST || process.env.MAIL_HOST,
   port: Number(env.MAIL_PORT || process.env.MAIL_PORT || 587),
@@ -8,6 +13,9 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: env.MAIL_USER || process.env.MAIL_USER,
     pass: env.MAIL_PASS || process.env.MAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
